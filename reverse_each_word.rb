c6916604b21c841602ca7313b2b490b9720e67e3
anya litvinova <anya.s.litvinova@gmail.com>
2019-08-14 22:03:19 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,17 @@
 def reverse_each_word(string)
- reversed_string = []
- 
- string.each do |char|
-  reversed_string.unshift(char)
- end
- 
- return reversed_string.join('')
+ original_array = string.split(" ")
+  return_array = []
+  original_array.each do|string|
+    return_array << string.reverse
+  end
+  return_array.join(" ")
+end
+
+def reverse_each_word(string)
+  array = string.split(" ") #turn string into an array
+  test_array = []
+  array.collect do|string| #iterate over the array
+    test_array << string.reverse #reverse each word in the array
+  end
+  test_array.join(" ")
 end
